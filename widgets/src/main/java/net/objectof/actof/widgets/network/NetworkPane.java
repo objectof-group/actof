ffcea87c4e48ff00e34d19de4faa0c677bd056c7
Nathaniel Sherry <nsherry4@uwo.ca>
2015-04-06 12:53:34 -0400

    NetworkPane smooth line shapes when moving nodes

--- widgets/src/main/java/net/objectof/actof/widgets/network/NetworkPane.java
+++ widgets/src/main/java/net/objectof/actof/widgets/network/NetworkPane.java
@@ -12,6 +12,8 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.binding.NumberBinding;
 import javafx.beans.binding.When;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
@@ -166,16 +168,28 @@ public class NetworkPane extends Pane {
         DoubleBinding dx = destX.subtract(sourceX);
         DoubleBinding dy = destY.subtract(sourceY);
         DoubleBinding slope = dx.divide(dy);
+        DoubleBinding invslope = dy.divide(dx);
+
+        DoubleProperty zero = new SimpleDoubleProperty(0);
+        DoubleProperty one = new SimpleDoubleProperty(1);
 
         // is the curve/line more horizontal or vertical
         BooleanBinding vertical = slope.greaterThan(1).or(slope.lessThan(-1));
 
+        // scaling function for control point coords which is maximized at 1.5
+        // at 0/90 degrees and minimized at 0 at 45
+        NumberBinding scale = new When(vertical).then(invslope).otherwise(slope);
+        scale = new When(scale.lessThan(0)).then(zero.subtract(scale)).otherwise(scale);
+        scale = one.subtract(scale);
+        scale = scale.multiply(1.5);
+
         // control point positions using boolean binding to change value based
-        // on value of variable 'vertical'
-        NumberBinding cx1 = new When(vertical).then(sourceX).otherwise(sourceX.add(dx.divide(3)));
-        NumberBinding cx2 = new When(vertical).then(destX).otherwise(destX.subtract(dx.divide(3)));
-        NumberBinding cy1 = new When(vertical).then(sourceY.add(dy.divide(3))).otherwise(sourceY);
-        NumberBinding cy2 = new When(vertical).then(destY.subtract(dy.divide(3))).otherwise(destY);
+        // on value of variable 'vertical'. We apply a non-zero control point
+        // coordinate to the dominant axis
+        NumberBinding cx1 = new When(vertical).then(sourceX).otherwise(sourceX.add(dx.multiply(scale)));
+        NumberBinding cx2 = new When(vertical).then(destX).otherwise(destX.subtract(dx.multiply(scale)));
+        NumberBinding cy1 = new When(vertical).then(sourceY.add(dy.multiply(scale))).otherwise(sourceY);
+        NumberBinding cy2 = new When(vertical).then(destY.subtract(dy.multiply(scale))).otherwise(destY);
 
         CubicCurveTo curve = new CubicCurveTo();
         curve.controlX1Property().bind(cx1);
