784f18a604d186051f1e1acd8ba74dc654c82988
Nathaniel Sherry <nsherry4@uwo.ca>
2015-06-12 11:29:13 -0400

    Added exception dialog method

---
+++ widgets/src/main/java/net/objectof/actof/widgets/ActofDialogs.java
@@ -0,0 +1,53 @@
+package net.objectof.actof.widgets;
+
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+
+public class ActofDialogs {
+
+    public static void exceptionDialog(Throwable ex) {
+
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(ex.getMessage());
+
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        String exceptionText = sw.toString();
+
+        Label label = new Label("The exception stacktrace was:");
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setPrefColumnCount(80);
+        textArea.setEditable(false);
+        textArea.setWrapText(false);
+        textArea.setStyle("-fx-font-family: monospace;");
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        alert.getDialogPane().setContent(expContent);
+
+        alert.showAndWait();
+
+    }
+
+}
