1dc06902aa86baca4805ba7088f287fe66f7f3de
Nathaniel Sherry <nsherry4@uwo.ca>
2015-08-31 10:34:50 -0400

    Updated IDE to new ActofWindow API

--- ide/src/main/java/net/objectof/actof/ide/ActofIDE.java
+++ ide/src/main/java/net/objectof/actof/ide/ActofIDE.java
@@ -12,17 +12,12 @@ public class ActofIDE extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        ActofWindow window = ActofWindow.load();
-        window.setDisplayStage(primaryStage);
-        window.construct();
-        window.getFXRegion().setPrefHeight(470);
-        window.getFXRegion().setPrefWidth(1000);
-
         Editor ide = ActofIDEController.load();
         ide.setDisplayStage(primaryStage);
         ide.construct();
 
-        window.setEditor(ide);
+        ActofWindow window = new ActofWindow(primaryStage, ide);
+        window.setSize(1000, 470);
         window.show();
 
     }
